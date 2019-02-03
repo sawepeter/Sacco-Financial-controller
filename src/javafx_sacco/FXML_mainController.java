e962e05eac6ed8653748dc276847fce7e2e7aedd
Iampato <858wpwaweru@gmail.com>
2019-02-03 23:55:56 +0300

    Added a login gif animation

--- src/javafx_sacco/FXML_mainController.java
+++ src/javafx_sacco/FXML_mainController.java
@@ -1,20 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javafx_sacco;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import backend.Notify;
 /**
  * FXML Controller class
  *
- * @author Kenya
+ * @author Pato
  */
 public class FXML_mainController implements Initializable {
+
+    @FXML
+    private AnchorPane MainWindow;
+    @FXML
+    private TextField first_name;
+    @FXML
+    private TextField second_name;
 
     /**
      * Initializes the controller class.
@@ -23,5 +29,48 @@ public class FXML_mainController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void HandleRegisterAction(ActionEvent event) {
+        Notify x = new Notify();
+
+        x.PopupAnimation("Registration", "Successfuly registered a new customer", "SUCCESS", 3);
+    }
+
+    @FXML
+    private void HandleCheckBalanceAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void HandleDepositCashAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void HandleDepositMpesaAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void HandleWithdrawAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void HandleApplyLoanAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void HandleEligibilityAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void HandlePayLoanCashAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void HandlePayLoanMpesaAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void HandleLogoutAction(ActionEvent event) {
+    }
     
 }
